22fb5a3ada9096421447a644f47ebf81448eb235
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-04-27 21:01:47 +0300

    refactor(domain): eliminate product retrieval from home repository

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,9 +1,7 @@
 import 'package:e_commerce/config/base_response/base_response.dart';
 import 'package:e_commerce/features/home/domain/entities/category_entity.dart';
-import 'package:e_commerce/features/home/domain/entities/product_entity.dart';
 
 abstract class HomeRepo {
-  Future<BaseResponse<List<ProductEntity>>> getProducts(int page, int pageSize);
   Future<BaseResponse<List<CategoryEntity>>> getCategories();
   Future<BaseResponse<CategoryEntity>> getCategoryById(String id);
 }
